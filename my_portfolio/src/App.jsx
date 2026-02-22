0e8172b2df885ce15686f999c559682b2b28fd8a
Your Name <shahbajhussain@gmail.com>
2026-02-22 16:11:26 +0530

    "app done"

--- my_portfolio/src/App.jsx
+++ my_portfolio/src/App.jsx
@@ -1,13 +1,29 @@
-
-import './App.css'
+import "./App.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/contact";
+import { Education } from "./components/Education/Education";
+import { Experience } from "./components/Experience/Experience";
+import { Footer } from "./components/Footer/Footer";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Skills } from "./components/Skills/Skills";
+import { Work } from "./components/Work/Work";
 
 function App() {
   return (
-    <div >
-      <h1 >were</h1>
+    <div className="bg-[#050414]">
+     <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      <div className="relative pt-20">
+        <Navbar />
+        <About />
+        <Skills />
+        <Experience />
+        <Work />
+        <Education />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
 }
 
-
-export default App
+export default App;

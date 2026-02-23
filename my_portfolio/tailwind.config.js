57a9d92e01876bcc2b9f1e40432097fd37777b18
Your Name <shahbajhussain@gmail.com>
2026-02-23 15:56:42 +0530

    add effects

--- my_portfolio/tailwind.config.js
+++ my_portfolio/tailwind.config.js
@@ -4,8 +4,21 @@ export default {
   theme: {
     extend: {
       
+      /* ===== Blob Animation ===== */
+      keyframes: {
+        blob: {
+          "0%": { transform: "scale(1)" },
+          "33%": { transform: "scale(1.2)" },
+          "66%": { transform: "scale(0.8)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+
+      animation: {
+        blob: "blob 10s infinite",
+      },
+
     },
   },
   plugins: [],
 };
-
